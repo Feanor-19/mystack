ee2e96987e3c77b5dfb752c1805db2cd98e1a292
Filipp <sap.phil.v@mail.ru>
2023-10-02 14:45:22 +0300

    мелкие правки

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ void inline print_elem_t(FILE *stream, Elem_t val) { fprintf(stream, "{i = %d, d
 
 #define STACK_DO_DUMP
 #define STACK_USE_POISON
-#define STACK_ABORT_ON_DUMP
+//#define STACK_ABORT_ON_DUMP
 #define STACK_DUMP_ON_INVALID_POP
 #define STACK_USE_PROTECTION_CANARY
 #define STACK_USE_PROTECTION_HASH
@@ -23,55 +23,55 @@ void inline print_elem_t(FILE *stream, Elem_t val) { fprintf(stream, "{i = %d, d
 
 int main()
 {
+
     Stack stk = {};
 
     printf("----ctor\n");
     stack_ctor(&stk);
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----push1\n");
     stack_push(&stk, {19, 19.19, 'f'});
-    //STACK_DUMP(&stk, 0);
-
+    STACK_DUMP(&stk, 0);
 
     printf("----pop1\n");
     fortest x = {};
     stack_pop(&stk, &x);
     print_elem_t(stdout, x);
     printf("\n");
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----push2\n");
     stack_push(&stk, {-1, 23.42, 's'});
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----push3\n");
     stack_push(&stk, {-1, 23.42, 's'});
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----push4\n");
     stack_push(&stk, {-1, 23.42, 's'});
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----push5\n");
     stack_push(&stk, {-1, 23.42, 's'});
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----push6\n");
     stack_push(&stk, {2, 0.01, 'k'});
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----pop2\n");
     stack_pop(&stk, &x);
     print_elem_t(stdout, x);
     printf("\n");
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     printf("----pop3\n");
     stack_pop(&stk, &x);
     print_elem_t(stdout, x);
     printf("\n");
-    //STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 0);
 
     stack_dtor(&stk);
 
